0a1dd9b9b47959ca95a3746661964908fa482ca3
Tekari <34735550+Tekariyaki@users.noreply.github.com>
2022-05-11 12:21:02 +1000

    Add files via upload

---
+++ SquidwardSupreme/Commands - OLD.cs
@@ -0,0 +1,33 @@
+﻿using Discord.Commands;
+using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SquidwardSupreme;
+using Discord;
+
+namespace SquidwardSupreme
+{
+    // Create a module with the '*' prefix
+    [Group("*")]
+    public class Commands : ModuleBase<SocketCommandContext>
+    {
+        [Command("r")]
+        [Summary("Returns a random anime image.")]
+        public async Task RandomImageAsync(int num = 0)
+        {
+            Console.WriteLine("*r command in progress.");
+            await ReplyAsync(embed: new EmbedBuilder { ImageUrl = Program.GetRandomImageDir() }.Build());
+        }
+
+        // ~say hello world -> hello world
+        [Command("say")]
+        [Summary("Echoes a message.")]
+        public Task SayAsync([Remainder] [Summary("The text to echo")] string echo)
+            => ReplyAsync(echo);
+
+        // ReplyAsync is a method on ModuleBase 
+    }
+}
---
+++ SquidwardSupreme/DiscordNET Commands.cs
@@ -0,0 +1,37 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.Webhook;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace SquidwardSupreme
+{
+    public class Commands : ModuleBase<SocketCommandContext>
+    {
+        [Command("ping")]
+        private async Task Ping()
+        {
+            await ReplyAsync("Pong! 🏓 **" + Program._client.Latency + "ms**");
+        }
+
+        [Command("r")]
+        [Summary("Returns a random anime image.")]
+        public async Task RandomImageAsync()
+        {
+            string @imageDir = Program.GetRandomImageDir();
+            //Console.WriteLine("Got file: " + imageDir);
+            //await ReplyAsync(embed: new EmbedBuilder { ImageUrl = @imageDir }.Build());
+
+            //var embed = new EmbedBuilder()
+            //{
+            //    ImageUrl = $"attachment://{Path.GetFileName(imageDir).ToString()}"}.Build();
+            //await Context.Channel.SendFileAsync(@imageDir, embed: embed);
+
+            await Context.Channel.SendFileAsync(@imageDir);
+
+
+            Console.WriteLine(@imageDir);
+        }
+    }
+}
